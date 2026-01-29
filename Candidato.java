317c16d6e30385d5e825e05ead4d8e7bb1353ba7
Victor de Oliveira <victoroliveira.arte@gmail.com>
2026-01-29 15:41:03 -0300

    Base classes created, and added Getters and Setters of Candidato class

---
+++ Candidato.java
@@ -0,0 +1,46 @@
+public class Candidato {
+    // Atributos
+    private String nome;
+    private int numero;
+    private int votos;
+
+    // Construtor
+    public Candidato(String nome, int numero) {
+        this.setNome(nome);
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome invalido");
+        }
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        if (numero < 0) {
+            throw new IllegalArgumentException("Numero invalido, o numero deve ser positivo");
+        }
+        this.numero = numero;
+    }
+
+    public int getVotos() {
+        return this.votos;
+    }
+
+    public void setVotos(int votos) {
+        if (votos < 0) {
+            throw new IllegalArgumentException(
+                    "Quantidade de Votos invalido, nao e permitido colocar um valor negativo");
+        }
+        this.votos = votos;
+    }
+}
---
+++ UrnaEletronicaJava.java
@@ -0,0 +1,5 @@
+public class UrnaEletronicaJava {
+    public static void main(String[] args) {
+       
+    }
+}
